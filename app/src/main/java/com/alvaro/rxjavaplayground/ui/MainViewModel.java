55a6fe0dc2b2f74ff89c573627646518b2746c58
Alvaro Isea <alvaroe9448@gmail.com>
2021-11-06 15:26:10 +0100

    used create operator

--- app/src/main/java/com/alvaro/rxjavaplayground/ui/MainViewModel.java
+++ app/src/main/java/com/alvaro/rxjavaplayground/ui/MainViewModel.java
@@ -2,11 +2,11 @@ package com.alvaro.rxjavaplayground.ui;
 
 import android.util.Log;
 
-import com.alvaro.rxjavaplayground.datasource.DummyDataSource;
 import com.alvaro.rxjavaplayground.model.Task;
 
 import androidx.lifecycle.ViewModel;
 import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -17,11 +17,17 @@ public class MainViewModel extends ViewModel {
 
     public Observable<Task> taskObservable;
 
-
     public void execute(){
         Log.d(TAG, "MainViewModel: triggered execute");
         taskObservable = Observable
-                .fromIterable(DummyDataSource.Companion.createList())
+                .create((ObservableOnSubscribe<Task>) emitter -> {
+                    if (!emitter.isDisposed()){
+                        emitter.onNext(
+                                new Task("Home work description" , false , 3)
+                        );
+                        emitter.onComplete();
+                    }
+                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
